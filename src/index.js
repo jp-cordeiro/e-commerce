fa81a77b13c30642e1c431971741e8403eea6f7c
JP <jpc.araujo@hotmail.com>
2020-10-18 20:37:26 -0300

    add basename in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/e-commerce/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
