375c2416631e05dd91a7189548b371b7da7f6edf
JP <jpc.araujo@hotmail.com>
2020-11-13 22:25:06 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
