6f90493537e6f3c91d010016b7791a0eef926a23
JP <jpc.araujo@hotmail.com>
2020-11-13 22:18:19 -0300

    update SignIn with CustomButton component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 
 import './sign-in.styles.scss';
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type="submit" value="Submit" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
