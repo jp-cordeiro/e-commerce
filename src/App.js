4549e89ece3e922998642ed62a1be34d628b558f
JP <jpc.araujo@hotmail.com>
2020-11-13 22:27:29 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
+        <Route path="/test" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
