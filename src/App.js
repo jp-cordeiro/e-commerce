bc3a38c97b221b5a2ed4abdb0347e56ed8152a46
JP <jpc.araujo@hotmail.com>
2020-11-13 22:29:25 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/test" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
